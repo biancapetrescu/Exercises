9f8e86d5e09240a426a2b7135cbcda51e7f0fc56
biancapetrescu <bianca.9672@yahoo.com>
2022-09-27 20:42:35 +0300

    Added problem description

--- ArraysAndStrings/IsUnique/src/Main.java
+++ ArraysAndStrings/IsUnique/src/Main.java
@@ -1,3 +1,6 @@
+/* Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+cannot use additional data structures? */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
